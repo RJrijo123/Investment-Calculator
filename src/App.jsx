422945cbdfafd0447977b515c2dfd99087ea8142
Rijo Jacob <rijo.jacob@imaginist.ai>
2025-01-07 12:25:21 +0530

    Fixed app with input Validation

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,39 @@ function App() {
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10,
-});
+  });
 
-function handleChange(inputIdentifier, newValue){
-    setUserInput(prevUserInput => {
-        return {
-            ...prevUserInput,
-            [inputIdentifier]: +newValue
-        }
-});
-}
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    const parsedValue = +newValue;
+
+    if (parsedValue < 0) {
+      setErrorMessage("Value must be positive");
+      return;
+    } else {
+      setErrorMessage(""); 
+    }
+
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: parsedValue,
+      };
+    });
+  }
+
   return (
     <>
-    <Header/>
-    <UserInput userInput={userInput} onChange={handleChange}/>
-    <Results input={userInput}/>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {errorMessage && <p className="center">{errorMessage}</p>}
+      {inputIsValid && !errorMessage && <Results input={userInput} />}
+      {!inputIsValid && <p className="center">Please Enter a Duration Greater than Zero</p>}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
